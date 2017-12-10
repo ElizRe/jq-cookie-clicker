ca9a3c6e62c02f070c5616613879bc625fb2f395
ElizRe <elizreed@laposte.net>
2017-12-10 19:24:10 +0100

    first add

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-	//votre code
+var randomMessages = ["clicking is bad for you","aren't you bored yet?","you should be studying"]
+var cookieCount = 0;
 
+$(document).ready(function(){
+
+	$(".cake").click(function() {
+		cookieCount += 1;
+		$("span").html(cookieCount);
+		if (cookieCount%20 == 0) { alert(randomMessages[randomIntFromInterval(0, (randomMessages.length - 1) )]  )}
+
+	});
 
 });
+
+
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
